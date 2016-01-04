72c2f3634ecc63c5203ef7b7490b70beb6d9f70f
赵佳慧--芒果iOS <1146623752@qq.com>
2016-01-04 15:04:40 +0800

    zhao

---
+++ WaitingWeekend/Class/Main/Controllers/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  WaitingWeekend
+//
+//  Created by scjy on 16/1/4.
+//  Copyright © 2016年 scjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
